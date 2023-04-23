30e1eb6fc98deb92a3f21132d797a0cbfaa6a408
rkcfkrrt <lwy12993@gmail.com>
2023-04-23 11:21:53 +0800

    更新 router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Search from '../components/Search.vue'
+Search
 
 Vue.use(VueRouter)
 
@@ -10,14 +12,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/Search',
+    name: 'Search',
+    component: Search
+  },
 ]
 
 const router = new VueRouter({
